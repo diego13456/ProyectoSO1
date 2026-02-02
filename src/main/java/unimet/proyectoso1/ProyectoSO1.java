890302f2cf5a08273baa9e0b2eece4d5d6128c73
diego13456 <156132250+diego13456@users.noreply.github.com>
2026-02-02 19:30:49 -0400

    Add files via upload

---
+++ src/main/java/unimet/proyectoso1/ProyectoSO1.java
@@ -0,0 +1,14 @@
+
+
+package unimet.proyectoso1;
+
+/**
+ *
+ * @author diego
+ */
+public class ProyectoSO1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
